68bc2efb056aeeabc4c19fb5e8077f2c64fa1959
dimidrol <dmitrijrostovcev878@gmail.com>
2024-06-09 19:29:17 +0300

    Version 3.1

--- Source1.cpp
+++ Source1.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
 
-int main() {
-	setlocale(LC_ALL, "rus");
-	const int maxSize = 100; // Максимальный размер массива
-	int n;
-	std::cout << "Введите размер массива (не более " << maxSize << "): ";
-	std::cin >> n;
-	if (n > maxSize || n <= 0) {
-		std::cout << "Ошибка: неверный размер массива\n";
+
 		return 1;
 }
