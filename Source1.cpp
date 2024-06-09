c77834154548f62b74931788ca38dfbf97f45c83
dimidrol <dmitrijrostovcev878@gmail.com>
2024-06-09 19:24:45 +0300

    Revert "Version 2.0"
    This reverts commit 8086744ea7f93ddfeb6d449bbd17512133353be2.
    
    # Conflicts:
    #	Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -14,4 +14,24 @@ int main() {
 		std::cout << "Ошибка: неверный размер массива\n";
 >>>>>>> b5f9417c962c8aebce77444e638140933538d57f
 		return 1;
+<<<<<<< HEAD
+=======
+	}
+
+	int arr[maxSize];
+	std::cout << "Введите элементы массива:\n";
+	for (int i = 0; i < n; ++i) {
+		std::cin >> arr[i];
+	}
+
+	selectionSort(arr, n);
+
+	std::cout << "Отсортированный массив:\n";
+	for (int i = 0; i < n; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+>>>>>>> parent of 8086744 (Version 2.0)
 }
